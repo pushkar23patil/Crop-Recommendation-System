80b4ad2712bea78cf3413e2379f0ea8c62cf2e99
pushkar patil <128344688+pushkar23patil@users.noreply.github.com>
2025-05-17 17:41:43 +0530

    Update app.py
    testing automatic sensor data

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas
-import sklearn
 import pickle
+import os
 
-model = pickle.load(open('model.pkl','rb'))
-sc = pickle.load(open('standscaler.pkl','rb'))
-mx = pickle.load(open('minmaxscaler.pkl','rb'))
-
+model = pickle.load(open('model.pkl', 'rb'))
+sc = pickle.load(open('standscaler.pkl', 'rb'))
+mx = pickle.load(open('minmaxscaler.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -15,35 +13,35 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/predict",methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    N = request.form['Nitrogen']
-    P = request.form['Phosporus']
-    K = request.form['Potassium']
-    temp = request.form['Temperature']
-    soil_moisture = request.form['soil_moisture']
+    try:
+        data = request.get_json()
+        N = float(data['Nitrogen'])
+        P = float(data['Phosporus'])
+        K = float(data['Potassium'])
+        temp = float(data['Temperature'])
+        soil_moisture = float(data['soil_moisture'])
+
+        feature_list = [N, P, K, temp, soil_moisture]
+        single_pred = np.array(feature_list).reshape(1, -1)
+
+        mx_features = mx.transform(single_pred)
+        sc_mx_features = sc.transform(mx_features)
+        prediction = model.predict(sc_mx_features)
+
+        crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                     8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                     14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                     19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+
+        crop = crop_dict.get(prediction[0], "Unknown")
+        return jsonify({"recommended_crop": crop})
     
-    feature_list = [N, P, K, temp, soil_moisture]
-    single_pred = np.array(feature_list).reshape(1, -1)
-
-    mx_features = mx.transform(single_pred)
-    sc_mx_features = sc.transform(mx_features)
-    prediction = model.predict(sc_mx_features)
-
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
-
-    if prediction[0] in crop_dict:
-        crop = crop_dict[prediction[0]]
-        result = "{} is the best crop to be cultivated right there".format(crop)
-    else:
-        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-    return render_template('index.html',result = result)
-
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    #app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
+
